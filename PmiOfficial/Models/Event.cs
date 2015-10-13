89525cd6ced3203d36af4bbd4da36cc44581a0d1
Max <yamax0312@gmail.com>
2015-10-13 22:05:14 +0300

    Revert "no message"
    This reverts commit 4fca18821ebcaafc1d32b05006a8e7e32d9a00a2.

--- PmiOfficial/Models/Event.cs
+++ PmiOfficial/Models/Event.cs
@@ -7,17 +7,5 @@ namespace PmiOfficial.Models
 {
     public class Event
     {
-        private int id;
-        public int ID { get { return id; } set { id = value; } }
-        private string name;
-        public string Name { get { return name; } set { name = value; } }
-        private string location;
-        public string Location { get { return location; } set { location = value; } }
-        private string description;
-        public string Decription { get { return description; } set { description = value; } }
-        private User organiser;
-        public User Organiser { get { return organiser; } set { organiser = value; } }
-        private DateTime time;
-        public DateTime Time { get { return time; } set { time = value; } }
     }
 }
