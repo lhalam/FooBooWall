dd50121c7e52d42b07c29023c320801f74a52c7c
Fakvarl <Skolandrik@gmail.com>
2015-10-24 17:52:44 +0300

    Cleaned project a bit

--- PmiOfficial/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace PmiOfficial.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
--- PmiOfficial/Startup.cs
+++ PmiOfficial/Startup.cs
@@ -12,7 +12,6 @@ namespace PmiOfficial
     {
         public void Configuration(IAppBuilder app)
         {
-            ConfigureAuth(app);
         }
     }
 }
