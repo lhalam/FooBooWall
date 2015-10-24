db358536c2e3f95587507f159f3824eacb4e71c9
kamu12 <yuriy.kaminskyi@gmail.com>
2015-10-24 09:18:57 +0300

    Update DB according to entity

--- PmiDatabase/Tables/events.sql
+++ PmiDatabase/Tables/events.sql
@@ -1,6 +1,10 @@
 ﻿CREATE TABLE events
 (
-	ID			int IDENTITY(1,1) PRIMARY KEY,
-	Image_id	int FOREIGN KEY REFERENCES images(ID),
-	Text		text
+	ID				int IDENTITY(1,1) PRIMARY KEY,
+	Name			text, 
+	Location		varchar(200),
+	Description		text,
+	Image_id		int FOREIGN KEY REFERENCES images(ID),
+	Organizer_id	int FOREIGN KEY REFERENCES users(ID),
+	Event_time		datetime
 )
