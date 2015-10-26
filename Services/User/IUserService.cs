5b7d5bf7a0819b0cc878b9a4182af3169dc56e3c
Andriy Medytsky <medandriy@gmail.com>
2015-10-26 02:19:57 +0200

    Implements UserService

---
+++ Services/User/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.DAO;
+using DataAccess.Entities;
+
+namespace Services
+{
+    public interface IUserService
+    {
+        User Get(int id);
+    }
+}
---
+++ Services/User/UserService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.DAO;
+using DataAccess.Entities;
+
+namespace Services
+{
+    public class UserService : IUserService
+    {
+        AbstractDAO<User> _dao;
+
+        public UserService(AbstractDAO<User> dao)
+        {
+            _dao = dao;
+        }
+
+        public User Get(int id)
+        {
+            return _dao.Read(id);
+        }
+    }
+}
