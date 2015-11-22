641669064cfbb1889426636230b9b8106c175a11
IvanIlnytskyi <i.ilnytskyi@gmail.com>
2015-11-22 16:57:29 +0200

    Added Comment entity

---
+++ DataAccess/Entities/Comment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Entities
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public int AuthorId { get;set;}
+        public string AuthorName { get; set; }
+        public string ImageName { get; set; }
+        public DateTime WritingDate { get; set; }
+        public string Text { get; set; }
+    }
+}
