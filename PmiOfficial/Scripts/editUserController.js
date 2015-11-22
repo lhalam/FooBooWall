ad48e603a5393be35bf654e1c6b3d9dc34106608
Taras Panychok <taraspanychok@mail.ua>
2015-11-22 23:28:57 +0200

    Add basic validation to editing profile

--- PmiOfficial/Scripts/editUserController.js
+++ PmiOfficial/Scripts/editUserController.js
@@ -7,15 +7,27 @@
         Birthday: new Date($("#BirthDate").val()).getTime()
     }
    
-    $.post("UserProfile/Edit", editedUser)
-      .success(function (data) {
-          alert("User was edited successfully!");
-          window.location.reload(true);
-      });
+    if (checkValid(editedUser)) {
+        $.post("UserProfile/Edit", editedUser)
+          .success(function (data) {
+              alert("User was edited successfully!");
+              window.location.reload(true);
+          });
+    } else {
+        alert("Wrong input!");
+    }
 }
 
-var pickDate = function () {
+var checkValid = function (editedUser) {
+    var emailRegexp = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+    var result = editedUser.FirstName.length > 0 &&
+                 editedUser.LastName.length > 0 &&
+                 emailRegexp.test(editedUser.EMail);
+    return result;
+}
+
+$(window).on('shown.bs.modal', function () {
     $('#BirthDate').datepick({
         maxDate: new Date()
     });
-}
+});
