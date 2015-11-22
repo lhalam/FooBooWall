d9849090efc72a3e68397fc41a32de5f45a7d66f
Maxym Yaromych <yamax0312@gmail.com>
2015-11-22 03:03:41 +0200

    Fix registration page.Add bootstrap styling, front-end validation(Kozub,Yaromych)

---
+++ PmiOfficial/Scripts/registration.js
@@ -0,0 +1,41 @@
+﻿$(function () {
+    $("#okbutton").click(function () {
+        if (validatePassword()) {
+            $.post("/api/Account/Register",
+                {
+                    Login: $("#login").val(),
+                    Password: $("#password").val(),
+                    Email: $("#email").val(),
+                }).then
+                (function (data) {
+                    alert("User registered successfully!");
+                    $("#list").text(' ');
+                    document.location.href = "/Login/Login";
+                }, function (error) {
+
+                    var errors = [];
+                    for (var key in error.responseJSON.ModelState) {
+                        for (var i = 0; i < error.responseJSON.ModelState[key].length; i++) {
+                            errors.push(error.responseJSON.ModelState[key][i])
+                        }
+                    }
+                    var message = errors.join(' ');
+                    $("#list").text(message);
+                }
+                );
+        }
+    });
+});
+
+function validatePassword()
+{
+    if ($("#rpassword").val() != $("#password").val()) {
+        $("#list").text("Passwords should match!");
+        return false;
+    }
+    else return true;
+}
+//стилі
+//валідація на фронтенді
+//кі по імейлу
+//перевід на логування
