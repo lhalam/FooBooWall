0a5022486b853aaf9245bcef80f23d189d5adb73
Igor Bandurovych <igor.bandurovych@gmail.com>
2015-12-06 22:01:25 +0200

    Added HTTPS enforcement for Web API
    Strict Transport Security Header and Cookies for HTTPS also added

---
+++ PmiOfficial/Filters/EnforceHttpsHandler.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace PmiOfficial.Filters
+{
+    public class EnforceHttpsHandler : DelegatingHandler
+    {
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            // If request is local, just serve it without HTTPS
+            object httpContextBaseObject;
+            if (request.Properties.TryGetValue("MS_HttpContext", out httpContextBaseObject))
+            {
+                var httpContextBase = httpContextBaseObject as HttpContextBase;
+
+                if (httpContextBase != null && httpContextBase.Request.IsLocal)
+                {
+                    return base.SendAsync(request, cancellationToken);
+                }
+            }
+
+            // If request is remote, enforce HTTPS
+            if (request.RequestUri.Scheme != Uri.UriSchemeHttps)
+            {
+                return Task<HttpResponseMessage>.Factory.StartNew(
+                    () =>
+                    {
+                        var response = new HttpResponseMessage(HttpStatusCode.Forbidden)
+                        {
+                            Content = new StringContent("HTTPS Required")
+                        };
+
+                        return response;
+                    });
+            }
+
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+}
