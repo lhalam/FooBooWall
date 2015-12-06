08df115f0754066eaef303ba12adc3af484537db
taras.antoshchuk <t.antoshchuk@gmail.com>
2015-12-06 10:55:18 +0200

    EventService basic implementation

---
+++ Services/Event/EventService.cs
@@ -0,0 +1,54 @@
+﻿using DataAccess.Entities;
+using DataAccess.DAO;
+using Services.DTO;
+
+namespace Services
+{
+    class EventService: IEventService
+    {
+        private EventDAO _dao;
+
+        public EventService(EventDAO dao) 
+        {
+            _dao = dao;        
+        }
+
+        public Event Get(int id)
+        {
+            return _dao.Read(id);
+        }
+
+        public void Edit(EventDTO eventDTO)
+        {
+            Event e = Get(eventDTO.Id);
+            e.Decription = eventDTO.Decription;
+            e.ImageId = eventDTO.ImageId;
+            e.Location = eventDTO.Location;
+            e.Name = eventDTO.Name;
+            e.Time = eventDTO.Time;
+
+            _dao.Update(e);
+        }
+
+        public void Delete(int id)
+        {
+            Event e = Get(id);
+
+            _dao.Delete(e);
+        }
+
+        public void Create(EventDTO eventDTO, int userId)
+        {
+            Event e = new Event();
+
+            e.Id = eventDTO.Id;
+            e.ImageId = eventDTO.ImageId;
+            e.Location = eventDTO.Location;
+            e.Name = eventDTO.Name;
+            e.OrganizerId = userId;
+            e.Time = eventDTO.Time;
+
+            _dao.Create(e);
+        }
+    }
+}
