b75136c0f8e6270bd690c6a2cffc01a70c45b6b4
kamu12 <yuriy.kaminskyi@gmail.com>
2015-12-06 21:23:15 +0200

    Add some test data

--- PmiDatabase/Script.Seed.sql
+++ PmiDatabase/Script.Seed.sql
@@ -10,13 +10,16 @@ Post-Deployment Script Template
 --------------------------------------------------------------------------------------
 */
 
-INSERT INTO images (Name)
+INSERT INTO images(Name) 
 VALUES('https://i.ytimg.com/vi/GlcaoHxi5p4/maxresdefault.jpg' );
-INSERT INTO images (Name)
+INSERT INTO images(Name) 
 VALUES('https://pp.vk.me/c628627/v628627737/a321/2uk5TUqgz6E.jpg' );
 
-INSERT INTO users(FirstName, LastName, Login, Email, BirthDate, Image_id)
-VALUES('John', 'McKein', 'YourTree', 'mcKeinTheKing@ukr.net', '1995-01-03', 1);
+INSERT INTO users(FirstName, LastName, Login, Email, Image_id)
+VALUES('John', 'McKein', 'YourTree', 'mcKeinTheKing@ukr.net', 1);
 
-INSERT INTO users(FirstName, LastName, Login, Email, BirthDate, Image_id)
-VALUES('Sofiya', 'Padus', 'YourQeen', 'sofija_padus@ukr.net', '1996-12-04', 2);
+INSERT INTO users(FirstName, LastName, Login, Email, Image_id)
+VALUES('Sofiya', 'Padus', 'YourQeen', 'sofija_padus@ukr.net', 2);
+
+INSERT INTO Events(Name, Location, Description, Event_time, Image_id, Organizer_id)
+VALUES('Equator selebration', 'Lviv', 'Celebration of half term of studying', '2016-01-04', 2, 2);
