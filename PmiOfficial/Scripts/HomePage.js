d81b985c173c2e6170228f6079e0db88ed9efa98
Yuriy <yuriy.pryyma@vakoms.com.ua>
2015-12-07 05:03:54 +0200

    1. add slider with news to home page 2. add snoop dog trap

---
+++ PmiOfficial/Scripts/HomePage.js
@@ -0,0 +1,32 @@
+﻿$(".copy_rigths").on('click', function (ev) {
+    if (!$("#left_dog").is(":visible")) {
+        $("#left_dog").show();
+        $("#right_dog").show();
+        $(".slider").hide();
+        $("#snop_song")[0].src += "&autoplay=1";
+        ev.preventDefault();
+        resizeColumns();
+    }
+    else {
+        $(".slider").show();
+        $("#left_dog").hide();
+        $("#right_dog").hide();
+        $("#snop_song")[0].src = "//www.youtube.com/embed/KlujizeNNQM?rel=0";
+        ev.preventDefault();
+        resizeColumns();
+    }
+});
+
+var $myCols = $("#myColumns");
+
+function resizeColumns() {
+    var visibleCols = $myCols.children(":visible").length;
+
+    var div = Math.floor(12 / visibleCols);
+    var rem = 12 % visibleCols;
+
+    var colSize = (rem === 0) ? div : 2;
+
+    $myCols.children().removeClass().addClass('col-md-' + colSize);
+
+}
