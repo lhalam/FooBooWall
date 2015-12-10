35aed261199ef8803b51663f2d9c861d2a4037c7
taras.antoshchuk <t.antoshchuk@gmail.com>
2015-12-11 01:11:50 +0200

    Switched from mock events to database events in EventController impl

--- PmiOfficial/Controllers/EventController.cs
+++ PmiOfficial/Controllers/EventController.cs
@@ -17,44 +17,16 @@ namespace PmiOfficial.Controllers
     {
         private EventService _eventService = new EventService(new EventDAO());
         private CommentService _commentsService = new CommentService(new EventCommentsDao());
+        private static int TEST_EVENT_ID = 1;
 
         public ActionResult Index(int? userId, int? eventId) 
         {
-            //uncomment when CommentsDao is implemented
+            int idOfEvent = eventId == null ? TEST_EVENT_ID : (int)eventId;
 
-            //ViewBag.Event = _eventService.Get(eventId);
-            //ViewBag.Comments = _commentsService.GetEventComments(eventId);
-            //ViewBag.UserId = userId;
+            ViewBag.Event = _eventService.Get(idOfEvent);
+            ViewBag.Comments = _commentsService.GetEventComments(idOfEvent);
+            ViewBag.UserId = userId;
 
-            Event dummyEvent = new Event();
-
-            dummyEvent.Decription = "This is event example";
-            dummyEvent.Id = 0;
-            dummyEvent.ImageId = 1001;
-            dummyEvent.Location = "Lwiw";
-            dummyEvent.Name = "event example";
-            dummyEvent.OrganizerId = 1;
-            dummyEvent.Time = DateTime.Now;
-
-            ViewBag.Event = dummyEvent;
-            Comment c1 = new Comment();
-            c1.AuthorId = 1;
-            c1.AuthorName = "Author 1";
-            c1.Id = 100;
-            c1.ImageName = "https://static.pexels.com/photos/6550/nature-sky-sunset-man.jpeg";
-            c1.WritingDate = DateTime.Now;
-            c1.Text = "Very nice comment 1";
-            Comment c2 = new Comment();
-            c1.AuthorId = 2;
-            c1.AuthorName = "Author 2";
-            c1.Id = 101;
-            c1.ImageName = "https://static.pexels.com/photos/6550/nature-sky-sunset-man.jpeg";
-            c1.WritingDate = DateTime.Now;
-            c1.Text = "Very nice comment 2";
-            List<Comment> c = new List<Comment>();
-            c.Add(c1);
-            c.Add(c2);
-            ViewBag.Comments = c;
             return View();
         }
 
@@ -67,6 +39,7 @@ namespace PmiOfficial.Controllers
             return RedirectToAction("Index", "UserProfile", new { userId = currentUserId });
         }
 
+        [HttpPost]
         public void AddComment(Comment comment)
         {
             _commentsService.Create(comment);
