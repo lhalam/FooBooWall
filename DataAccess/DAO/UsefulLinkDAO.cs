8823c1e238f7f80f6806e88ae485c86ff857161c
Taras Panychok <taraspanychok@mail.ua>
2015-12-11 17:53:59 +0200

    Refactored DAO

--- DataAccess/DAO/UsefulLinkDAO.cs
+++ DataAccess/DAO/UsefulLinkDAO.cs
@@ -21,13 +21,13 @@ namespace DataAccess.DAO
                 string sql =
                     "INSERT INTO usefulLinks(User_Id, Url, Image_Id, Comment, Name)" +
                     " OUTPUT Inserted.Id " +
-                    "VALUES(@param1, @param2, @param3, @param4, @param5)";
+                    "VALUES(@User_Id, @Url, @Image_Id, @Comment, @Name)";
                 SqlCommand cmd = new SqlCommand(sql, connection);
-                cmd.Parameters.Add(new SqlParameter("@param1", entity.OwnerUserID));
-                cmd.Parameters.Add(new SqlParameter("@param2", entity.Url ?? SqlString.Null));
-                cmd.Parameters.Add(new SqlParameter("@param3", entity.ImageId ?? SqlInt32.Null));
-                cmd.Parameters.Add(new SqlParameter("@param4", entity.Comment ?? SqlString.Null));
-                cmd.Parameters.Add(new SqlParameter("@param5", entity.Name ?? SqlString.Null));
+                cmd.Parameters.Add(new SqlParameter("@User_Id", entity.OwnerUserID));
+                cmd.Parameters.Add(new SqlParameter("@Url", entity.Url ?? SqlString.Null));
+                cmd.Parameters.Add(new SqlParameter("@Image_Id", entity.ImageId ?? SqlInt32.Null));
+                cmd.Parameters.Add(new SqlParameter("@Comment", entity.Comment ?? SqlString.Null));
+                cmd.Parameters.Add(new SqlParameter("@Name", entity.Name ?? SqlString.Null));
                 cmd.CommandType = CommandType.Text;
                 entity.Id = (int) cmd.ExecuteScalar();
                 return;
@@ -40,20 +40,20 @@ namespace DataAccess.DAO
             using (SqlConnection connection = GetConnection())
             {
                 connection.Open();
-                string sql = "SELECT * FROM usefulLinks WHERE Id = @param1";
+                string sql = "SELECT * FROM usefulLinks WHERE Id = @Id";
                 SqlCommand cmd = new SqlCommand(sql, connection);
-                cmd.Parameters.Add(new SqlParameter("@param1", id));
+                cmd.Parameters.Add(new SqlParameter("@Id", id));
                 using (SqlDataReader reader = cmd.ExecuteReader())
                 {
                     reader.Read();
                     usefulLink = new UsefulLink
                     {
                         Id = id,
-                        Name = (string) reader.GetValue(1),
-                        OwnerUserID = (int) reader.GetValue(2),
-                        Url = (string) reader.GetValue(3),
-                        ImageId = (int) reader.GetValue(4),
-                        Comment = (string) reader.GetValue(5)
+                        Name = Convert(reader["Name"]),
+                        OwnerUserID = (int) reader["User_Id"],
+                        Url = Convert(reader["Url"]),
+                        ImageId = (int) reader["Image_Id"],
+                        Comment = Convert(reader["Comment"])
                     };
                 }
             }
@@ -65,15 +65,15 @@ namespace DataAccess.DAO
             using (SqlConnection connection = GetConnection())
             {
                 connection.Open();
-                string sql = "UPDATE usefulLinks SET OwnerUserID = @param1, Url = @param2, ImageId = @param3," +
-                             " Comment = @param4, Name = @param5 WHERE Id = @param6";
+                string sql = "UPDATE usefulLinks SET OwnerUserID = @OwnerUserID, Url = @Url, ImageId = @ImageId," +
+                             " Comment = @Comment, Name = @Name WHERE Id = @Id";
                 SqlCommand cmd = new SqlCommand(sql, connection);
-                cmd.Parameters.Add(new SqlParameter("@param1", entity.OwnerUserID));
-                cmd.Parameters.Add(new SqlParameter("@param2", entity.Url));
-                cmd.Parameters.Add(new SqlParameter("@param3", entity.ImageId));
-                cmd.Parameters.Add(new SqlParameter("@param4", Convert(entity.Comment)));
-                cmd.Parameters.Add(new SqlParameter("@param5", entity.Name));
-                cmd.Parameters.Add(new SqlParameter("@param6", entity.Id));
+                cmd.Parameters.Add(new SqlParameter("@OwnerUserID", entity.OwnerUserID));
+                cmd.Parameters.Add(new SqlParameter("@Url", entity.Url));
+                cmd.Parameters.Add(new SqlParameter("@ImageId", entity.ImageId));
+                cmd.Parameters.Add(new SqlParameter("@Comment", Convert(entity.Comment)));
+                cmd.Parameters.Add(new SqlParameter("@Name", entity.Name));
+                cmd.Parameters.Add(new SqlParameter("@Id", entity.Id));
                 cmd.CommandType = CommandType.Text;
                 cmd.ExecuteNonQuery();
             }
@@ -84,9 +84,9 @@ namespace DataAccess.DAO
             using (SqlConnection connection = GetConnection())
             {
                 connection.Open();
-                string sql = "DELETE FROM usefulLinks WHERE Id = @param1";
+                string sql = "DELETE FROM usefulLinks WHERE Id = @Id";
                 SqlCommand cmd = new SqlCommand(sql, connection);
-                cmd.Parameters.Add(new SqlParameter("@param1", entity.Id));
+                cmd.Parameters.Add(new SqlParameter("@Id", entity.Id));
                 cmd.CommandType = CommandType.Text;
                 cmd.ExecuteNonQuery();
             }
@@ -106,12 +106,12 @@ namespace DataAccess.DAO
                     {
                         UsefulLink usefulLink = new UsefulLink
                         {
-                            Id = (int)reader.GetValue(0),
-                            Name = Convert(reader.GetValue(1)),
-                            OwnerUserID = (int)reader.GetValue(2),
-                            Url = Convert(reader.GetValue(3)),
-                            ImageId = (int)reader.GetValue(4),
-                            Comment = Convert(reader.GetValue(5))
+                            Id = (int)reader["Id"],
+                            Name = Convert(reader["Name"]),
+                            OwnerUserID = (int)reader["User_Id"],
+                            Url = Convert(reader["Url"]),
+                            ImageId = (int)reader["Image_Id"],
+                            Comment = Convert(reader["Comment"])
                         };
                         result.Add(usefulLink);
                     }
