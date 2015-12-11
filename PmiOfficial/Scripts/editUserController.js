5963ed09e6ba9e4879d9e810a0df500bc8047bdf
Taras Panychok <taraspanychok@mail.ua>
2015-12-11 19:20:50 +0200

    Add cool js validation using regexps

--- PmiOfficial/Scripts/editUserController.js
+++ PmiOfficial/Scripts/editUserController.js
@@ -8,10 +8,9 @@
         SkypeName: $("#SkypeName").val()
     }
 
-    var emailRegexp = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
-    var result = editedUser.FirstName.length > 0 &&
-                 editedUser.LastName.length > 0 &&
-                 emailRegexp.test(editedUser.EMail);
+    var result = checkFirstName(editedUser.FirstName) &&
+                 checkLastName(editedUser.LastName) &&
+                 checkEmail(editedUser.EMail);
 
     if (result) {
         $("#BirthDate").val(editedUser.Birthday);
@@ -19,6 +18,34 @@
 
 
     return result;
+}
+
+var EMAIL_REGEX = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+var FIRST_LAST_NAME_REGEX = /^([A-Z\u0410-\u042f\u0407\u0406\u0404']{1}[a-z\u0430-\u044f\u0456\u0457\u0454']{1,20}\u002d{1}[A-Z\u0410-\u042f\u0407\u0406\u0404']{1}[a-z\u0430-\u044f\u0456\u0457\u0454']{1,20}|[A-Z\u0410-\u042f\u0407\u0406\u0404']{1}[a-z\u0430-\u044f\u0456\u0457\u0454']{1,20})$/;
+
+
+var checkFirstName = function (firstName) {
+    if (!FIRST_LAST_NAME_REGEX.test(firstName)) {
+        alert("There is an error in first name!");
+        return false;
+    }
+    return true;
+}
+
+var checkLastName = function (lastName) {
+    if (!FIRST_LAST_NAME_REGEX.test(lastName)) {
+        alert("There is an error in last name!");
+        return false;
+    }
+    return true;
+}
+
+var checkEmail = function (email) {
+    if (!EMAIL_REGEX.test(email)) {
+        alert("There is an error in email!");
+        return false;
+    }
+    return true;
 }
 
 $(function () {
